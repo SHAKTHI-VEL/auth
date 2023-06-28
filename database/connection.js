43b165ae15d31fd044a54810faa4399aa32b6c66
Shakthivel <93870684+SHAKTHI-VEL@users.noreply.github.com>
2023-06-28 20:12:48 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 dotenv.config({path:'./config/config.env'});
-const url=process.env.url
+const url="mongodb+srv://linux:PucHlDb2E6Slcbup@cluster0.wvzbn2c.mongodb.net/user?retryWrites=true&w=majority"
 // Function:-Connect to Databse
 const connection=()=>{
     mongoose.connect(url,{useNewUrlParser:true})
